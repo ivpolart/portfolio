449006efac5234eae58f52abe84b2a23dae8f1ba
Ivan Polozkov <ivpolart@gmail.com>
2026-01-26 21:01:12 +0200

    Update

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,7 +1,7 @@
 import fallbackImage from "../images/img-02.webp";
 import positivusImg from "../images/pp-positivus.png";
 import shebaImg from "../images/pp-sheba.png";
-// import portfolioImg from "../images/portfolio-preview.png";
+import gamesImg from "../images/pp-games.png";
 
 export { fallbackImage };
 
@@ -9,43 +9,43 @@ export const projects = [
   {
     id: 1,
     title: "Landing Page",
-    githubUrl: "https://github.com/ivpolart/LP_Positivus",
-    techStack: ["HTML", "CSS", "jQuery", "JS", "SCSS"],
-    previewImage: positivusImg,
+    githubUrl: "https://ivpolart.github.io/LP_Gamers_Platform/",
+    techStack: ["HTML", "CSS", "JS"],
+    previewImage: gamesImg,
   },
   {
     id: 2,
     title: "Landing Page",
-    githubUrl: "https://github.com/ivpolart/LP_Sheba",
-    techStack: ["HTML", "CSS", "jQuery", "JS", "SCSS"],
-    previewImage: shebaImg,
+    githubUrl: "https://github.com/ivpolart/LP_Positivus",
+    techStack: ["HTML", "CSS", "JS"],
+    previewImage: positivusImg,
   },
   {
     id: 3,
-    title: "Portfolio Website",
-    githubUrl: "https://github.com/ivpolart/portfolio",
-    techStack: ["React", "Styled Components"],
-    previewImage: "./images/portfolio-preview.png",
+    title: "Landing Page",
+    githubUrl: "https://github.com/ivpolart/LP_Sheba",
+    techStack: ["HTML", "CSS", "JS"],
+    previewImage: shebaImg,
   },
   {
     id: 4,
     title: "Portfolio Website",
     githubUrl: "https://github.com/ivpolart/portfolio",
     techStack: ["React", "Styled Components"],
-    previewImage: "./images/portfolio-preview.png",
+    previewImage: fallbackImage,
   },
   {
     id: 5,
     title: "Portfolio Website",
     githubUrl: "https://github.com/ivpolart/portfolio",
     techStack: ["React", "Styled Components"],
-    previewImage: "./images/portfolio-preview.png",
+    previewImage: fallbackImage,
   },
   {
     id: 6,
     title: "Portfolio Website",
     githubUrl: "https://github.com/ivpolart/portfolio",
     techStack: ["React", "Styled Components"],
-    previewImage: "./images/portfolio-preview.png",
+    previewImage: fallbackImage,
   },
 ];
