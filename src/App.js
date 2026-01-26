8e80ec386c4f892aded6abb2a426d4911dbf3a81
Ivan Polozkov <ivpolart@gmail.com>
2026-01-26 21:21:57 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ import Footer from "./components/Footer"
 function App() {
     return (
       <div className="App">
-        <HashRouter basename="/portfolio">
+        <HashRouter>
           <Header />
           <main id="main">
           <Routes>
