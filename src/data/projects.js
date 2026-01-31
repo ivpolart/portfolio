930c2b2fd5d99d6f82d058123591828d0ebb971e
Ivan Polozkov <ivpolart@gmail.com>
2026-01-31 23:17:34 +0200

    Update

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,7 +2,9 @@ import fallbackImage from "../images/img-02.webp";
 import positivusImg from "../images/pp-positivus.png";
 import shebaImg from "../images/pp-sheba.png";
 import gamesImg from "../images/pp-games.png";
-import todolistImg from "../images/pp-pp-todolist.png";
+import todolistImg from "../images/pp-todolist.png";
+import podcastImg from "../images/pp-podcast.png";
+import neonImg from "../images/pp-neon.png";
 
 export { fallbackImage };
 
@@ -24,29 +26,29 @@ export const projects = [
   {
     id: 3,
     title: "Landing Page",
-    githubUrl: "https://ivpolart.github.io/LP_Sheba/",
+    githubUrl: "https://github.com/ivpolart/LP_Gamers_Platform",
     techStack: ["HTML", "CSS", "JS"],
-    previewImage: shebaImg,
+    previewImage: podcastImg,
   },
   {
     id: 4,
     title: "To do list",
     githubUrl: "https://ivpolart.github.io/To-do-List/",
-    techStack: [HTML, CSS, JS],
+    techStack: ["Vanilla JS"],
     previewImage: todolistImg,
   },
   {
     id: 5,
-    title: "Portfolio Website",
-    githubUrl: "https://github.com/ivpolart/portfolio",
-    techStack: ["React", "Styled Components"],
-    previewImage: fallbackImage,
+    title: "Landing Page",
+    githubUrl: "https://ivpolart.github.io/To-do-List/",
+    techStack: ["HTML", "CSS", "JS"],
+    previewImage: neonImg,
   },
   {
     id: 6,
-    title: "Portfolio Website",
-    githubUrl: "https://github.com/ivpolart/portfolio",
-    techStack: ["React", "Styled Components"],
-    previewImage: fallbackImage,
+    title: "Landing Page",
+    githubUrl: "https://ivpolart.github.io/LP_Sheba/",
+    techStack: ["HTML", "CSS", "JS"],
+    previewImage: shebaImg,
   },
 ];
